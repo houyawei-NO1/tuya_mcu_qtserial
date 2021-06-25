12cbfcb49896e83341126213c76ec0ed34bca0da
houyawei <30817974+houyawei-NO1@users.noreply.github.com>
2021-06-25 17:08:43 +0800

    datapoint up

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ static QSerialPort *SerialTuya;
 
 void StartSerialTuya()
 {
-    qDebug()<<"##########################StartSerialTuya"<<endl;
+    qDebug()<<"StartSerialTuya"<<endl;
     SerialTuya = new QSerialPort();
     if(SerialTuya->isOpen())
     {
@@ -73,8 +73,7 @@ void SerialMean(QString read)
 {
 
     //tuya moudle send head(55aa)+version(00)+command(xx)+datalen(xx)+data(xx...)+datacheck(xx)
-//    read = "55aa00000000ff";//tuya moudle heartcheck
-     read = "55aa0001000000";//tuya moudle productcheck
+
     //heartcheck comand(0x00)
     bool static heartcheck = true;
     if(read.indexOf("55aa0000")==0)
@@ -103,26 +102,46 @@ void SerialMean(QString read)
         qDebug()<<"ProductMessageBytes:"<<ProductMessageBytes<<endl;
         QByteArray SendToTuya = SendData(QByteArray::fromHex("01"),ProductMessageBytes,true);
     }
-    //operation mode
-     else if(read.indexOf("55aa0003")==0)
+    //operation mode comand(0x02)
+     else if(read.indexOf("55aa0002")==0)
     {
-         SerialTuya->write(QByteArray::fromHex("55aa0303000005"));
+         QByteArray SendToTuya = SendData(QByteArray::fromHex("02"),"0c0d",true);
+//         SerialTuya->write(QByteArray::fromHex("55aa0303000005"));
+          qDebug()<<"operation mode"<<SendToTuya;
     }
-    //Data Point
+
+    //wifi status comand(0x03)
+    else if(read.indexOf("55aa0003")==0)
+    {
+        QByteArray SendToTuya = SendData(QByteArray::fromHex("03"),"",true);
+//         SerialTuya->write(QByteArray::fromHex("55aa0303000005"));
+         qDebug()<<"wifi status"<<SendToTuya;
+    }
+
+     //Data Point up
+    else if(read.indexOf("55aa0008")==0)
+    {
+//        datapoint  dpid05 type02 len0004 value0000001e
+        QByteArray SendToTuya = SendData(QByteArray::fromHex("07"),"050200040000001e",true);
+//        datapointarray[0]  dpid109 6d type01 len0001 value01
+//        datapointarray[1]  dpid102 66 type03 len000c value323031383034313231353037
+//         SerialTuya->write(QByteArray::fromHex("55aa0303000005"));
+         qDebug()<<"Data Point up"<<SendToTuya;
+
+         QByteArray SendToTuyaarray = SendData(QByteArray::fromHex("07"),"6d010001016603000c323031383034313231353037",true);
+
+    }
  return;
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-//    StartSerialTuya();
-//    QString str = "{“p”:”RN2FVAgXG6W****”,“v”:”1.0.0”,”m”}";
-//    qDebug()<<str<<endl;
-//    QByteArray ascii = str.toUtf8().toHex();
-//    qDebug()<<ascii<<endl;
-//   QByteArray SendToTuya = SendData(QByteArray::fromHex("00"),"00",true);
-    SerialMean("test1");
-    SerialMean("test2");
+    SerialMean("55aa00000000ff");//tuya moudle heartcheck
+    SerialMean("55aa0001000000");//tuya moudle productcheck
+    SerialMean("55aa0002000001");//tuya moudle operation modecheck
+    SerialMean("55aa000300010003");//tuya moudle wifi status
+    SerialMean("55aa0008000007");
     return a.exec();
 }
 
